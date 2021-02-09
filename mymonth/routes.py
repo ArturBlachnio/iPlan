8ad96830c113ce58359e699d1660c323187ec534
Artur Blachnio <blachnio.artur@gmail.com>
2021-02-09 16:46:07 +0100

    Display table finished

--- mymonth/routes.py
+++ mymonth/routes.py
@@ -2,11 +2,11 @@ from flask import render_template
 from mymonth import db
 from mymonth import app
 from mymonth.models import Days
-from mymonth.utils import get_month_first_and_last_day, get_all_days_of_month
+from mymonth.utils import get_month_days
 from datetime import date
 
  
-REF_DATE = date(2021, 3, 2)
+REF_DATE = date(2021, 2, 2)
 
 
 @app.route('/home')
@@ -17,12 +17,12 @@ def main():
 
 @app.route('/')
 def home():
-    month_start, month_end = get_month_first_and_last_day(REF_DATE)
-    all_days_of_month = get_all_days_of_month(REF_DATE)
+    month_start, month_end, month_all_days = get_month_days(REF_DATE)
+
     days = Days.query.filter(Days.id >= month_start).filter(Days.id <= month_end).all()
-    days_in_db = [day.id for day in days]
     # Add a day to database if it does not exist yet. 
-    for day_index in all_days_of_month:
+    days_in_db = [day.id for day in days]
+    for day_index in month_all_days:
         if day_index not in days_in_db:
             db.session.add(Days(id=day_index))
         db.session.commit()
--- mymonth/utils.py
+++ mymonth/utils.py
@@ -9,9 +9,17 @@ def get_month_first_and_last_day(reference_date):
     return first_day, last_day
 
 
-def get_all_days_of_month(reference_date):
+def get_month_all_days(reference_date):
     """Returns all days in a month (to be used as index in table)."""
     month_start, month_end = get_month_first_and_last_day(reference_date)
     days_in_month = (month_end - month_start).days + 2
     list_of_days = [date(year=month_start.year, month=month_start.month, day=day) for day in range(1, days_in_month)]
     return list_of_days
+
+def get_month_days(reference_date):
+    """Returns: first, last and all days of a month of reference date."""
+    first_day = date(year=reference_date.year, month=reference_date.month, day=1)
+    last_day = date(year=reference_date.year, month=reference_date.month, day=monthrange(year=reference_date.year, month=reference_date.month)[1])
+    days_in_month = (last_day - first_day).days + 2
+    all_days = [date(year=first_day.year, month=first_day.month, day=day) for day in range(1, days_in_month)]
+    return first_day, last_day, all_days
